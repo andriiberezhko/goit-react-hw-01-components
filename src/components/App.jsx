9091a84c2054fb63635ee81e774cb4941a241f23
Andrii Berezhko <Berezhko7777@ukr.net>
2022-06-07 17:46:02 +0300

    4
    4 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,9 @@ import data from './Statistics/data.json'
 import Statistics from "./Statistics/Statistics";
 import friends from "./FriendList/friends.json";
 import FriendList from "./FriendList/FriendList";
-// import FriendListItem from "./FriendListItem/FriendListItem";
+import transactions from './TransactionHistory/transactions.json'
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+
 
 
 
@@ -32,7 +34,7 @@ export const App = () => {
         stats={data}
       />
       <FriendList friends={friends} />
-
+      <TransactionHistory items={transactions} />
      
     </div>
   );
